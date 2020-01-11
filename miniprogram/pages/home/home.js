640335a2763869faca00404e92070e6252dbf3c6
supanpanCn <1372978934@qq.com>
2020-01-11 23:46:57 +0800

    初始化

---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,24 @@
+
+Page({
+
+  
+  data: {
+
+  },
+  navigateToAdd(){
+    wx.navigateTo({
+      url: '/pages/add/add'
+    })
+  },
+  navigateToindex(){
+    wx.navigateTo({
+      url: '/pages/index/index'
+    })
+  },
+  navigateToComplex(){
+    wx.navigateTo({
+      url: '/pages/complex/complex'
+    })
+  }
+ 
+})
