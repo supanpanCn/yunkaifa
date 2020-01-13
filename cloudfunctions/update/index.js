7b809edd96a842b141260862221b078cd8c45eda
supanpanCn <1372978934@qq.com>
2020-01-14 00:42:22 +0800

    收藏over

--- cloudfunctions/update/index.js
+++ cloudfunctions/update/index.js
@@ -6,20 +6,29 @@ const db = cloud.database()
 const _ = db.command
 // 云函数入口函数
 exports.main = async (event) => {
+  // db.collection(event.type.toUpperCase()).where({
+  //   index:1
+  // }).update({
+  //   data:{
+  //     collect:false
+  //   }
+  // }).then(res=>{
+  //   console.log(res)
+  // })
   // 更新收藏
-  let update_col = ()=>{
+  let update_col = async ()=>{
     return await db.collection(event.type.toUpperCase()).where({
-      collect:false
+      index:event.index
     }).update({
       data:{
-        collect:true
+        collect:event.col
       }
     })
   }
   // 重置
   let reset = ()=>{
     let arr = ["CSS","HTML","ES6","JQ","JS","REACT","VUE","WEIXIN"]
-    arr.forEach(v=>{
+    arr.forEach(async v=>{
       await db.collection(v).where({
         collect:true
       }).update({
@@ -35,11 +44,18 @@ exports.main = async (event) => {
   }else{
     res = update_col()
   }
-  
-  if(res.stats.updated){
-    return true
-  }else{
-    return false
-  }
+  return res.then(reslt=>{
+    if(reslt.stats.updated){
+      return true
+    }else{
+      return false
+    }
+  })
+  // return true
+  // if(res.stats.updated){
+  //   return true
+  // }else{
+  //   return false
+  // }
   
 }
